69861922d077313cf262bc2372637f615ee4ab2b
luisachica <lfchica94@gmail.com>
2025-12-11 14:07:24 -0500

    cambios en el middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -47,7 +47,6 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-    "/api/:path*",
+    "/((?!_next/static|_next/image|favicon.ico|api/auth/register|api/auth/login).*)",
   ],
 }
